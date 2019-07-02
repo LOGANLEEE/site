a8fe7ffdb21ab910134b319979b5f8d8facf063a
Logan Lee <dlfjgkssk1@naver.com>
2019-07-02 10:59:12 +0900

    Updates

---
+++ precache-manifest.d78e52004dfd0061a57f8ff1a91c5a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96dc4428d84c558ee4123ba0c9c72f19",
+    "url": "/react-git-page/index.html"
+  },
+  {
+    "revision": "2cf0dab7a1387e8fca13",
+    "url": "/react-git-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-git-page/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2cf0dab7a1387e8fca13",
+    "url": "/react-git-page/static/js/main.934009d4.chunk.js"
+  },
+  {
+    "revision": "3073c9715d0f2b58f402",
+    "url": "/react-git-page/static/js/runtime~main.4fd68763.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-git-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
